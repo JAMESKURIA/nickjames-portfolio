bcd8b8d79d7ff80c6241b5012894f168e820787f
CHEGE JAMES KURIA <jameskuria04@gmail.com>
2024-02-18 00:34:33 +0300

    Update about section

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'James Kuria Chege Peronal Portfolio Website',
+    description:
+      'My name is James Kuria Chege and I am a software engineer who enjoys building tech solutions for businesses and individuals. Check out my portfolio for more information about me and my work. Hire me for your next project and let me help you bring your ideas to life',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
@@ -17,10 +18,11 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@nickjames_dev" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
+        <link rel="icon" href="/kc-logo-light.png" />
       </Head>
       <body>
         <Main />
